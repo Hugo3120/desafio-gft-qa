5227e0ab034dd9898005f140226e92f94241190e
Hugo3120 <hugoleo3120@gmail.com>
2022-03-18 10:34:11 -0300

    Desafios BootCamp GFT QA

---
+++ src/Divisores.java
@@ -0,0 +1,23 @@
+/* Ler um número inteiro N e calcular todos os seus divisores.
+ ENTRADA
+ O arquivo de entrada contém um valor inteiro.
+ SAÍDA
+ Escreva todos os divisores positivos de N, um valor por linha.*/
+import java.util.Scanner;
+
+
+public class Divisores{
+    //complete os espaços em branco com sua solução
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+
+        for (int i = 1;i <= n; i++) {
+            if (n % i == 0) {
+                System.out.println( i );
+            }
+        }
+    }
+
+}
---
+++ src/DuracaoJogo.java
@@ -0,0 +1,32 @@
+/* Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração
+ do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro,
+ tendo uma duração mínima de 1 hora e máxima de 24 horas.
+
+ ENTRADA
+ A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
+ SAÍDA
+ Apresente a duração do jogo conforme exemplo abaixo.
+
+* */
+import java.util.Scanner;
+public class DuracaoJogo {
+    public static void main(String[] args){
+
+        //complete os espaços em branco com sua solução para o problema
+
+        Scanner leitor = new Scanner(System.in);
+
+        int hInicial = leitor.nextInt();
+        int hFinal = leitor.nextInt();
+
+        if (hInicial > hFinal) {
+            System.out.println("O JOGO DUROU " + (24 - (hInicial - hFinal)) + " HORA(S)");
+        } else if (hFinal > hInicial) {
+            System.out.println("O JOGO DUROU " + (hFinal - hInicial) + " HORA(S)");
+        } else {
+            System.out.println("O JOGO DUROU 24 HORA(S)");
+        }
+
+    }
+
+}
